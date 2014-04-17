30d1e6c289ed5d4084bd7c9ef904c47255e63218
rainfly <xiechc@gmail.com>
2014-04-17 14:16:33 +0800

    Update udp.c

--- common/udp.c
+++ common/udp.c
@@ -1,3 +1,23 @@
+/*
+  * A generic udp implementation
+  *
+  * Copyright (C) 2008 rainfly123 <xiechc@gmail.com>
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program; if not, write to the Free Software
+  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  *
+  */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,7 +31,7 @@ int32_t sdk_udp_socket(void)
     return socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP );
 }
 
-/* Ïú»Ùsocket */
+/* ï¿½ï¿½ï¿½ï¿½socket */
 void sdk_udp_close( int32_t skt )
 {
     if (skt != -1 )
