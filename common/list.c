48dc0be0a130807251b4766e652648cf32c47317
rainfly <xiechc@gmail.com>
2014-04-17 14:11:26 +0800

    Update list.c

--- common/list.c
+++ common/list.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2006  Adam Charrett
+Copyright (C) 2014 xiechc@gmail.com
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
