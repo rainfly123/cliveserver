8a30c353c85fe82b3d2a44431a773b019f07bd18
rainfly <xiechc@gmail.com>
2014-04-21 14:15:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
- * twemproxy - A fast and lightweight proxy for memcached protocol.
- * Copyright (C) 2011 Twitter, Inc.
+ * cliveserver - A fast live broadcast server ,supporting mulit-platform viewing.
+ * Copyright (C) rainfly xiechc@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
