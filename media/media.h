fc597d68439c89b22e020659c46942c7fd2f1a78
rainfly <xiechc@gmail.com>
2014-04-21 10:56:18 +0800

    Update media.h

--- media/media.h
+++ media/media.h
@@ -1,10 +1,30 @@
+/*
+  * A generic media implementation
+  *
+  * Copyright (C) 2014 rainfly123 <xiechc@gmail.com>
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program; if not, write to the Free Software
+  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  *
+  */
 #ifndef _CLIVE_MEDIA_H_
 #define _CLIVE_MEDIA_H_
 
 #include "kfifo.h"
 #include "list.h"
 
-enum {
+enum Media_Type {
     TS2TS = 1,
     TS2FLV = 2,
     FLV2FLV = 3,
