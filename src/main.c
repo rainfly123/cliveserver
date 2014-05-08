b7c9d8b725d99c8e1ba7e59f9e4e63b4e541d2be
rainfly123 <xiechc@gmail.com>
2014-05-08 18:36:24 +0800

    json

--- src/main.c
+++ src/main.c
@@ -6,15 +6,155 @@
 #include "media.h"
 #include "channel.h"
 #include "http.h"
+#include "jsmn.h"
 
-int main(int argc, char *argv)
+
+char * KEYS[] = {"channels", "channel_name", "input_url", "output"};
+
+static bool json_token_streq(char *js, jsmntok_t *t, char *s)
+{
+    return (strncmp(js + t->start, s, (size_t)(t->end - t->start)) == 0
+            && (int32_t)strlen(s) == (t->end - t->start));
+}
+
+static char * json_token_tostr(char *js, jsmntok_t *t)
+{
+    js[t->end] = '\0';
+    return js + t->start;
+}
+
+static char *read_cfg(const char *file_name)
+{
+    FILE *cfg;
+    size_t size;
+    char *buffer;
+    size_t got;
+
+    cfg  = fopen(file_name, "r");
+    if (cfg == NULL) {
+        log_error("open config file failed");
+        exit(-1);
+    }
+    fseek(cfg, 0, SEEK_END);
+    size = (size_t)ftell(cfg);
+    fseek(cfg, 0, SEEK_SET);
+    buffer = (char *) malloc(size);
+    got = fread(buffer, 1, size, cfg);
+    if (got != size) {
+        log_error("read config file failed");
+        exit(-1);
+    }
+    fclose(cfg);
+    return buffer;
+}
+
+static jsmntok_t * parse_cfg(char *buffer)
+{
+    uint32_t n  = 256;
+    int32_t ret;
+    jsmn_parser parser;
+
+    jsmn_init(&parser);
+    jsmntok_t *tokens = malloc(sizeof(jsmntok_t) * n);
+    ret = jsmn_parse(&parser, buffer, strlen(buffer), tokens, n);
+    while (ret == JSMN_ERROR_NOMEM)
+    {
+        n = n * 2 + 1;
+        tokens = realloc(tokens, sizeof(jsmntok_t) * n);
+        ret = jsmn_parse(&parser, buffer, strlen(buffer), tokens, n);
+    }
+
+    if ((ret == JSMN_ERROR_INVAL) || (ret == JSMN_ERROR_PART)) {
+        log_error("jsmn_parse: invalid JSON string");
+        exit(-1);
+    }
+
+    return tokens;
+}
+
+int main(int argc, char **argv)
 {
     struct sigaction sa;
+    jsmntok_t * tokens;
+    char *buffer;
 
     sa.sa_handler = SIG_IGN;
     sigaction(SIGPIPE, &sa, 0 );
 
     log_init(LOG_VERB, NULL);
     log_debug(LOG_DEBUG, "cliveserver starting..");
+    buffer = read_cfg("cliveserver.conf");
+    tokens = parse_cfg(buffer);
 
+    typedef enum { START, KEY, PRINT, SKIP} parse_state;
+    parse_state state = START;
+
+    int object_tokens = 0;
+    int i = 0, j = 1;
+    for (; j > 0; i++, j--)
+    {
+        jsmntok_t *t = &tokens[i];
+        if ((t->start == -1) || (t->end == -1))
+            return -1;
+        if (t->type == JSMN_ARRAY || t->type == JSMN_OBJECT)
+            j += t->size;
+
+        switch (state)
+        {
+            case START:
+                if (t->type != JSMN_OBJECT) {
+                    return -1;
+                }
+                state = KEY;
+                object_tokens = t->size;
+
+                if (object_tokens == 0)
+                    break;
+                if (object_tokens % 2 != 0)
+                    break;
+
+            case KEY:
+                object_tokens--;
+
+                if (t->type != JSMN_STRING)
+                    break;
+                state = SKIP;
+                size_t m;
+                for (m = 0; m < sizeof(KEYS)/sizeof(char *); m++)
+                {
+                    if (json_token_streq(buffer, t, KEYS[m]))
+                    {
+                        printf("%s: ", KEYS[m]);
+                        state = PRINT;
+                        break;
+                    }
+                }
+                break;
+
+            case SKIP:
+                if (t->type != JSMN_STRING && t->type != JSMN_PRIMITIVE)
+                    log_error("Invalid response: object values must be strings or primitives.");
+
+                object_tokens--;
+                state = KEY;
+
+                if (object_tokens == 0)
+                    break;
+
+            case PRINT:
+                if (t->type != JSMN_STRING && t->type != JSMN_PRIMITIVE)
+                    log_error("Invalid response: object values must be strings or primitives.");
+
+                char *str = json_token_tostr(buffer, t);
+                puts(str);
+
+                object_tokens--;
+                state = KEY;
+
+                if (object_tokens == 0)
+                    break;
+
+        }
+    }
+    return 0;
 }
